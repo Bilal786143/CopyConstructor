36eb929327f401ee963605515bcfdc3ea14bfc71
Muhammad bilal <meharbilal24525@gmail.com>
2023-12-03 13:22:46 +0500

    Add project files.

---
+++ CopyConstructor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+    class Constructor 
+    {
+        int age;
+        string name;
+        public Constructor()
+        {
+            Console.WriteLine("This is default Constructor");
+        }
+        public Constructor(int age,string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public Constructor(Constructor c)
+        {
+            this.age = c.age;
+            this.name = c.name;
+        }
+
+
+        public void showresult()
+        {
+            Console.WriteLine("The age is: {0} and name is : {1}",this.age,this.name);
+        }
+    
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Constructor c = new Constructor(24,"Bilal");
+            c.showresult();
+            Constructor n = new Constructor(c);
+            n.showresult();
+            
+            Console.ReadLine();
+        }
+    }
+}
